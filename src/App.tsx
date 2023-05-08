cf0b00a1a75ce568759829b47c772edfd64dc804
Ryan Turner <RyanTurner02@protonmail.com>
2023-05-08 14:33:07 -0400

    Create functional timer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Timer from './components/Timer';
 
 function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      <Timer />
     </h1>
   );
 }
---
+++ src/components/Timer.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+const Timer = () => {
+  const [minutes, setMinutes] = useState(0);
+
+  let currentDate: Date;
+  let nextDate: Date;
+
+  const updateDates = () => {
+    currentDate = new Date();
+
+    nextDate = new Date();
+    nextDate.setDate(nextDate.getDate() + 1);
+    nextDate.setHours(0);
+    nextDate.setMinutes(0);
+    nextDate.setSeconds(0);
+    nextDate.setMilliseconds(0);
+  };
+
+  const calculateTime = () => {
+    updateDates();
+
+    let startTime: number = currentDate.getTime();
+    let endTime: number = nextDate.getTime();
+    let deltaTime: number = endTime - startTime;
+    
+    setMinutes(Math.floor(deltaTime / 60000));
+  };
+
+  useEffect(() => {
+    const interval = setInterval(calculateTime, 1000);
+    
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      {minutes}
+    </div>
+  );
+}
+
+export default Timer;
