8e9acb3524048f37bb3c628563a03ec31de78437
Ryan Turner <RyanTurner02@protonmail.com>
2023-05-10 13:16:13 -0400

    Finish 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,10 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
   return (
-    <div>
+    <div className="m-8 text-center">
+      <h1 className="font-semibold text-6xl">404 Not Found</h1>
+      <p className="mt-4">Take me back to the <Link to="/" className="underline">Timer</Link>!</p>
     </div>
   );
 }
