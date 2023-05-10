aa3cb6462a09ece86cae3e9fc69a514d7d15de01
Ryan Turner <RyanTurner02@protonmail.com>
2023-05-10 15:26:45 -0400

    Change theme value between components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import './App.css';
+import { useState, createContext } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Timer from './components/Timer';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 
+export const ThemeContext = createContext("light");
+
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((theme === "light") ? "dark" : "light");
+  };
+
   return (
     <BrowserRouter>
-      <Header />
-      <div className="container mx-auto">
-        <Routes>
-          <Route path="/" element={<Timer />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
+      <ThemeContext.Provider value={theme}>
+        <Header toggleTheme={toggleTheme} />
+        <div className={`container mx-auto ${theme}`}>
+
+          {theme}
+
+          <Routes>
+            <Route path="/" element={<Timer />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </ThemeContext.Provider>
     </BrowserRouter>
   );
 }
