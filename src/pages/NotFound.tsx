e8d55037c3dd69789768a05b0047c9e3f920da1f
Ryan Turner <RyanTurner02@protonmail.com>
2023-05-10 17:45:23 -0400

    Make homepage link span across the entire paragraph tag

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div className="m-8 text-center">
       <h1 className="font-semibold text-6xl">404 Not Found</h1>
-      <p className="mt-4">Take me back to the <Link to="/" className="underline">Timer</Link>!</p>
+      <p className="mt-4"><Link to="/" className="underline">Take me back to the Timer!</Link></p>
     </div>
   );
 }
