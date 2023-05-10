f1f4b362fb97fcd0ec0dea19b54c59a83455c68a
Ryan Turner <RyanTurner02@protonmail.com>
2023-05-10 12:52:41 -0400

    Add routes and about and 404 pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Timer from './components/Timer';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="container mx-auto">
-      <Timer />
-    </div>
+    <BrowserRouter>
+      <div className="container mx-auto">
+        <Routes>
+          <Route path="/" element={<Timer />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
