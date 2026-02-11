c4add4e4036541230e50144167101f59a65a2724
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2026-02-10 21:40:44 -0600

    Update margin

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div className="m-8 text-center">
+    <div className="mt-16 text-center sm:mt-44">
       <h1 className="font-semibold text-8xl">404 Not Found</h1>
       <p className="mt-2 text-3xl">The requested page does not exist</p>
       <p className="text-3xl underline"><Link to="/">Back to the timer</Link></p>
